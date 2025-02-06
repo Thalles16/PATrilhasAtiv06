f782a47516c992839255dd72d4550524f63f2fec
Thalles <168739769+Thalles16@users.noreply.github.com>
2025-02-06 08:38:07 -0300

    questao A.cs

---
+++ questao A.cs
@@ -0,0 +1,19 @@
+using System;
+	
+
+class Program
+{
+   public static void Main()
+    {
+    	for (int i = 0; i < 100; i += 2)
+    	{
+    		Console.WriteLine(i);
+    		
+    	}
+    	Console.WriteLine("APERTE QUALQUER COISA PARA FECHAR");
+    	Console.ReadKey(true);
+
+        
+        
+    }
+}
